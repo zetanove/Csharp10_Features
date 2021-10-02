8e899036c2811f7409ab2cadee4b07df830bb74e
zetanove <antonio.pelleriti@gmail.com>
2021-10-02 17:12:32 +0200

    sealed ToString in record

---
+++ Csharp10_new_features/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//sealed ToString in record
+Figlia t = new Figlia("nome");
+Console.WriteLine(t.ToString());
+public record Persona(string Name)
+{
+    public sealed override string ToString() => "Io sono "+Name;
+}
+
+public record Figlia(string Name) : Persona(Name)
+{
+}
+
+//static abstract interface
